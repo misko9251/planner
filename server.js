e18d1783d1b8298233953da42576e6d1bb5d1298
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-07-17 14:20:44 -0400

    added env module

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const app = express();
 const mongoose = require('mongoose');
 const homeRoute = require('./routes/home');
 
+require('dotenv').config({path: './config/.env'})
+
+app.set('view engine', 'ejs')
+
 app.use('/', homeRoute);
 
 app.listen(process.env.PORT, ()=>{
-    console.log('We are running.');
-})
+    console.log(`Running on Port ${process.env.PORT}`)
+})  
